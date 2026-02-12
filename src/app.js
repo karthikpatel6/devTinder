447bb4fd8c909d75f6bfc4560b1b02b79d21181c
karthikpatel6 <karthikkpatel06@gmail.com>
2026-02-12 14:34:16 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -1,122 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/database'); // Ensure database is connected
 const app = express();
-const User = require('./models/user');
-const validateSignUpData  = require('./utils/validation');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-const { userAuth } = require('./middlewares/auth');
 
-
-const bcrypt = require('bcrypt');
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(cookieParser());
 
-app.post("/signup", async (req,res) => {
-    // console.log("Request body:", req.body); // Log the request body to see what data is being sent
+const authRouter = require('./routes/auth');
+const profileRouter = require('./routes/profile');
+const requestRouter = require('./routes/request');
 
-
-    // const userObj = {
-    //     firstName: "Karthik",
-    //     lastName: "Patel",
-    //     emailId: "karthik.patel@example.com",
-    //     password: "karthik@123"
-    // } 
-    // // Creating a new instance of the User model
-    // const user = new User(userObj);
-    // or
-    // const user = new User({
-    //     firstName: "Virat",
-    //     lastName: "Kohli",
-    //     emailId: "virat.kohli@example.com",
-    //     password: "virat@123",
-    //     age: 35,
-    //     gender: "Male"
-    // });
-    // or
-
-    try {
-    // validate the data
-    console.log("before validation");
-    validateSignUpData(req);
-    console.log("after validation");
-
-    const { firstName, lastName, emailId, password } = req.body;
-
-    // Encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash);
-
-    //Creating a new instance of the User model using the request body
-    const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash,
-    });
-
-        await user.save();
-        res.send("User created successfully");
-    } catch (err) {
-        res.status(400).send("Error creating user : " + err.message);
-    }
-});
-
-app.post("/login", async (req,res) => {
-    try{
-        const { emailId,password} = req.body;
-        
-        const user = await User.findOne({ emailId: emailId });
-        if(!user){
-            throw new Error("User not found");
-        }
-        const isPasswordValid = await user.validatePassword(password);
-        if(isPasswordValid){
-
-            // Create a JWT Token
-            const token = await user.getjwt();
-
-            // Add the token to cookie and send the response back to the user
-            res.cookie("token", token, {
-                expires: new Date(Date.now() + 8*360000),
-            });
-
-            res.send("Login successful");
-        }else{
-            throw new Error("Invalid password");
-        }
-    }catch(err){
-        res.status(400).send("Error logging in : " + err.message);
-    }
-});
-
-app.get("/profile", async (req,res) => {
-    try{
-        // const cookies = req.cookies;
-        // const { token } = cookies;
-        // // Validate the token
-        // if(!token){
-        //     throw new Error("Token is not valid");
-        // }
-        // const decodedMessage = await jwt.verify(token, "DevTinder$676");
-        // console.log("Decoded JWT token:", decodedMessage);
-        // const { _id } = decodedMessage;
-        // console.log("User ID from decoded token:", _id);
-        const user = req.user;
-        res.send(user);
-
-    }catch(err){
-        res.status(400).send("Error fetching profile : " + err.message);
-    }
-})
-
-app.post("/sendConnectionRequest",userAuth, async (req,res) => {
-    const user = req.user;
-    console.log("Sending connection request");
-
-    res.send(user.firstName + " sent a connection request");
-})
+app.use('/', authRouter);
+app.use('/', profileRouter);
+app.use('/', requestRouter);
 
 // GET user by email API
 app.get("/user", async (req,res) => {
---
+++ src/routes/auth.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const authRouter = express.Router();
+
+const validateSignUpData = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+
+authRouter.post("/signup", async (req,res) => {
+    try {
+    // validate the data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    //Creating a new instance of the User model using the request body
+    const user = new User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash,
+    });
+
+        await user.save();
+        res.send("User created successfully");
+    } catch (err) {
+        res.status(400).send("Error creating user : " + err.message);
+    }
+});
+
+authRouter.post("/login", async (req,res) => {
+    try{
+        const { emailId,password} = req.body;
+        
+        const user = await User.findOne({ emailId: emailId });
+        if(!user){
+            throw new Error("User not found");
+        }
+        
+        const isPasswordValid = await user.validatePassword(password);
+        if(isPasswordValid){
+
+            // Create a JWT Token
+            const token = await user.getjwt();
+
+            // Add the token to cookie and send the response back to the user
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8*360000),
+            });
+
+            res.send("Login successful");
+        }else{
+            throw new Error("Invalid password");
+        }
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+authRouter.post("/logout", (req,res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+    });
+    res.send("Logout successful");
+});
+
+module.exports = authRouter;
